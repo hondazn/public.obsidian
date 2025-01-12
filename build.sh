5e2b5eba593b2c3280719e662004f8dd315d2a22
HONDA Zyun <foundamn@gmail.com>
2025-01-13 03:46:29 +0900

    update build script

--- build.sh
+++ build.sh
@@ -4,5 +4,5 @@ git clone https://github.com/jackyzha0/quartz.git --depth 1
 cd quartz/
 npm ci
 npx quartz create -X new -l shortest
-mv -r ../notes/* content/
+find .. -mindepth 1 -maxdepth 1 -type d ! -name ".*" -exec mv {} content/ \;
 npx quartz build
